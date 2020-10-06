97a821625cc6624975bc245eaa8eabf2e04ad35b
Piotr Leś <->
2020-10-06 15:23:31 +0200

    Caluclate statistics, calculateStudentStatistics ovveride

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,42 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Not enough students, at least 5 required");
+            }
+            var twPercent = (int)Math.Ceiling(Students.Count * 0.2);
+            // var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            return 'a';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5 )
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");   
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name); 
+            }
+        }
+
         
     }
 }
