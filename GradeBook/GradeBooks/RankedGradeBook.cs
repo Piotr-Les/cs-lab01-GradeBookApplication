43fee8a1b4c1cf30dd7bf913ccfc88ac0beed05e
Piotr Leś <->
2020-10-06 20:12:56 +0200

    all tests done exept last one

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,8 +18,19 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Not enough students, at least 5 required");
             }
             var twPercent = (int)Math.Ceiling(Students.Count * 0.2);
-            // var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            return 'a';
+            var grades = Students.OrderByDescending(student => student.AverageGrade).Select(student =>student.AverageGrade ).ToList();
+            if (grades[twPercent-1] <= averageGrade)
+                return 'A';
+            if (grades[(twPercent * 2) -1] <=averageGrade)
+                // zniżamy do B ( index * 2) i -1 bo index zaczyna od 0
+                return 'B';
+            if (grades[(twPercent * 3) -1] <=averageGrade)
+                //analogicznie zniżamy do C ( index tabicy * 3) i -1 bo index zaczyna od 0
+                return 'C';
+            if(grades[(twPercent * 4) -1] <=averageGrade)
+                return 'D';
+            return 'F';
+
         }
 
         public override void CalculateStatistics()
